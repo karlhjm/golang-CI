42c97bd5ddb8c24b3a6fe07d645937699d211838
huangjm <1259040327@qq.com>
2018-06-30 16:13:25 +0800

    Update initial.go

--- db/initial.go
+++ db/initial.go
@@ -15,7 +15,7 @@ func checkErr(err error) {
 }
 
 func init() {
-	db, err := xorm.NewEngine("mysql", "root:mysql@tcp(localhost:3306)/hello2?charset=utf8&parseTime=true")
+	db, err := xorm.NewEngine("mysql", "root:@tcp(localhost:3306)/hello2?charset=utf8&parseTime=true")
 	checkErr(err)
 	engine = db
 	db.Sync2(new(entity.Restaurant), new(entity.Menufood), new(entity.Orders), new(entity.Orderfood), new(entity.Comment), new(entity.Categorys))
